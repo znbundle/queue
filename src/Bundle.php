30ed601fef1ad36e90360aabfecea385bea419b1
Vitaliy <theyamshikov@yandex.ru>
2021-01-26 00:26:23 +0600

    upd

---
+++ src/Bundle.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ZnBundle\Queue;
+
+use ZnCore\Base\Libs\App\Base\BaseBundle;
+
+class Bundle extends BaseBundle
+{
+
+    public function i18next(): array
+    {
+        return [
+            
+        ];
+    }
+
+    public function migration(): array
+    {
+        return [
+            '/vendor/znbundle/queue/src/Domain/Migrations',
+        ];
+    }
+
+    public function container(): array
+    {
+        return [
+            [__DIR__ . '/Domain/config/container.php', 'singletons'],
+        ];
+    }
+}
